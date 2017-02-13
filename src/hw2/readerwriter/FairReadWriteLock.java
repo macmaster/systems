45553f9c328b2029bdfbbcee2a9e8ff6c291af45
ronny <ronnymacmaster@gmail.com>
2017-02-12 21:21:12 -0600

    synchronized readwrite

--- src/hw2/readerwriter/FairReadWriteLock.java
+++ src/hw2/readerwriter/FairReadWriteLock.java
@@ -3,31 +3,26 @@ package hw2.readerwriter;
 import java.util.concurrent.Semaphore;
 
 class FairReadWriteLock {
-    int numReaders = 0;
-    Semaphore mutex = new Semaphore(1);
+    private int numReaders = 0;    
     Semaphore wlock = new Semaphore(1);
 
-    public void beginRead() throws InterruptedException {
-        mutex.acquire();
+    public synchronized void beginRead() throws InterruptedException {
         numReaders++;
         if (numReaders == 1)
             wlock.acquire();
-        mutex.release();
     }
 
-    public void endRead() throws InterruptedException {
-        mutex.acquire();
+    public synchronized void endRead() throws InterruptedException {
         numReaders--;
         if (numReaders == 0)
             wlock.release();
-        mutex.release();
     }
 
-    public void beginWrite() throws InterruptedException {
+    public synchronized void beginWrite() throws InterruptedException {
         wlock.acquire();
     }
 
-    public void endWrite() {
+    public synchronized void endWrite() {
         wlock.release();
     }
 }
