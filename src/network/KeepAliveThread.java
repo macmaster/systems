9d46935912121796714d8d151cb0df363feb47f9
John Doe <tschmidt64@utexas.edu>
2017-03-20 22:55:43 -0500

    KeepAliveThread.java finished, untested

--- src/network/KeepAliveThread.java
+++ src/network/KeepAliveThread.java
@@ -1,7 +1,31 @@
 package network;
 
+import java.io.*;
+
 /**
  * Created by tschmidt on 3/20/17.
  */
 public class KeepAliveThread extends Thread {
+    private PrintWriter pw;
+    private boolean alive = true;
+
+    public KeepAliveThread(PrintWriter pw) {
+        this.pw = pw;
+    }
+
+    public void kill() { self.alive = false; }
+
+    public void run() { keepConnectionAlive(); }
+
+    private void keepConnectionAlive() {
+        while (true) {
+            if (!alive) { break; }
+            try {
+                Thread.sleep(50);
+                pw.println("ping");
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
 }
