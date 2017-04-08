2c1f171d1e3c435f1f08e1e67d58d1783981f217
ronny <ronnymacmaster@gmail.com>
2017-04-08 03:07:14 -0500

    main.jar

--- src/TextAnalyzer.java
+++ src/TextAnalyzer.java
@@ -1,84 +1,101 @@
-import org.apache.hadoop.fs.Path;
+
+/** TextAnalyzer.java
+ * By: Taylor Schmidt and Ronald Macmaster
+ * UT-EID: trs2277   and    rpm953
+ * Date: 4/09/17
+ * 
+ * Hadoop MapReduce job.
+ */
+
+import java.io.*;
+
 import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.*;
 import org.apache.hadoop.mapreduce.*;
-import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
-import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
-import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
-import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
-
-import org.apache.hadoop.util.Tool;
-import org.apache.hadoop.util.ToolRunner;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
 
 // Do not change the signature of this class
 public class TextAnalyzer extends Configured implements Tool {
 
-    // Replace "?" with your own output key / value types
     // The four template data types are:
-    //     <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
-    public static class TextMapper extends Mapper<LongWritable, Text, ?, ?> {
-        public void map(LongWritable key, Text value, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you mapper function
-        }
-    }
-
-    // Replace "?" with your own key / value types
-    // NOTE: combiner's output key / value types have to be the same as those of mapper
-    public static class TextCombiner extends Reducer<?, ?, ?, ?> {
-        public void reduce(Text key, Iterable<Tuple> tuples, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you combiner function
-        }
-    }
-
-    // Replace "?" with your own input key / value types, i.e., the output
-    // key / value types of your mapper function
-    public static class TextReducer extends Reducer<?, ?, Text, Text> {
-        private final static Text emptyText = new Text("");
-
-        public void reduce(Text key, Iterable<Tuple> queryTuples, Context context)
-            throws IOException, InterruptedException
-        {
-            // Implementation of you reducer function
-
-            // Write out the results; you may change the following example
-            // code to fit with your reducer function.
-            //   Write out the current context key
-            context.write(key, emptyText);
-            //   Write out query words and their count
-            for(String queryWord: map.keySet()){
-                String count = map.get(queryWord).toString() + ">";
-                queryWordText.set("<" + queryWord + ",");
-                context.write(queryWordText, new Text(count));
+    // <Input Key Type, Input Value Type, Output Key Type, Output Value Type>
+    // reference:
+    // https://learnhadoopwithme.wordpress.com/tag/writablecomparable/
+    public static class TextMapper extends Mapper<LongWritable, Text, Text, CountPair> {
+        public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+            // cleans the line. then maps a word count.
+            String line = value.toString();
+            line.replaceAll("[^A-Za-z0-9]", " ");
+            String[] words = line.split("[^A-Za-z0-9]+");
+            for (int i = 0; i < words.length; i++) {
+                Text contextWord = new Text(words[i]);
+                for (int j = 0; j < words.length; j++) {
+                    if (i != j) { // tally query word
+                        Text queryWord = new Text(words[j]);
+                        LongWritable count = new LongWritable(1);
+                        context.write(contextWord, new CountPair(queryWord, count));
+                    }
+                }
             }
-            //   Empty line for ending the current context key
-            context.write(emptyText, emptyText);
+        }
+    }
+
+    // combiner's output key/value types have to be the same as those of mapper
+    public static class TextCombiner extends Reducer<Text, CountPair, Text, CountPair> {
+        public void reduce(Text key, Iterable<CountPair> tuples, Context context) throws IOException, InterruptedException {
+            System.out.println(key.toString());
+            for (CountPair tuple : tuples) {
+                System.out.format("combiner: (%s, %s)", key.toString(), tuple.toString());
+            }
+        }
+    }
+
+    // input is the output key / value types of your mapper function
+    public static class TextReducer extends Reducer<Text, CountPair, Text, Text> {
+        public void reduce(Text key, Iterable<CountPair> queryTuples, Context context) throws IOException, InterruptedException {
+            // // Implementation of you reducer function
+            //
+            // // Write out the results; you may change the following example
+            // // code to fit with your reducer function.
+            // // Write out the current context key
+            // context.write(key, emptyText);
+            // // Write out query words and their count
+            // for (String queryWord : map.keySet()) {
+            // String count = map.get(queryWord).toString() + ">";
+            // queryWordText.set("<" + queryWord + ",");
+            // context.write(queryWordText, new Text(count));
+            // }
+            // // Empty line for ending the current context key
+            // context.write(emptyText, emptyText);
+
+            System.out.println(key.toString());
+            for (CountPair tuple : queryTuples) {
+                System.out.format("reducer: (%s, %s)", key.toString(), tuple.toString());
+            }
         }
     }
 
     public int run(String[] args) throws Exception {
         Configuration conf = this.getConf();
 
-        // Create job
-        Job job = new Job(conf, "EID1_EID2"); // Replace with your EIDs
+        // Create and setup MapReduce job
+        @SuppressWarnings("deprecation")
+        Job job = new Job(conf, "rpm953_trs2277"); // Replace with your EIDs
         job.setJarByClass(TextAnalyzer.class);
-
-        // Setup MapReduce job
         job.setMapperClass(TextMapper.class);
-        //   Uncomment the following line if you want to use Combiner class
-        // job.setCombinerClass(TextCombiner.class);
+        job.setCombinerClass(TextCombiner.class);
         job.setReducerClass(TextReducer.class);
 
-        // Specify key / value types (Don't change them for the purpose of this assignment)
+        // Specify key / value types
+        // change if mapper and combiner's output type different from Text
+        job.setMapOutputKeyClass(Text.class);
+        job.setMapOutputValueClass(CountPair.class); // value is a count pair
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
-        //   If your mapper and combiner's  output types are different from Text.class,
-        //   then uncomment the following lines to specify the data types.
-        //job.setMapOutputKeyClass(?.class);
-        //job.setMapOutputValueClass(?.class);
 
         // Input
         FileInputFormat.addInputPath(job, new Path(args[0]));
@@ -86,6 +103,7 @@ public class TextAnalyzer extends Configured implements Tool {
 
         // Output
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        FileSystem.get(conf).delete(new Path(args[1]), true);
         job.setOutputFormatClass(TextOutputFormat.class);
 
         // Execute job and return status
@@ -94,15 +112,83 @@ public class TextAnalyzer extends Configured implements Tool {
 
     // Do not modify the main method
     public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new TextAnalyzer(), args);
-        System.exit(res);
+        int result = ToolRunner.run(new Configuration(), new TextAnalyzer(), args);
+        System.exit(result);
     }
 
-    // You may define sub-classes here. Example:
-    // public static class MyClass {
-    //
-    // }
+    /**
+     * Writable container for a comparable pair of (text, long).
+     * reference: https://learnhadoopwithme.wordpress.com/tag/writablecomparable/
+     */
+    public static class CountPair implements WritableComparable<CountPair> {
+        public Text text;
+        public LongWritable count;
+
+        /**
+         * Constructs a new Pair object. (left, right) <br>
+         */
+        public CountPair(Text text, LongWritable count) {
+            set(text, count);
+        }
+
+        /**
+         * sets the count pair values. <br>
+         */
+        public void set(Text text, LongWritable count) {
+            this.text = text;
+            this.count = count;
+        }
+
+        /**
+         * deserialize the object.
+         */
+        @Override
+        public void readFields(DataInput input) throws IOException {
+            this.text.readFields(input);
+            this.count.readFields(input);
+        }
+
+        /**
+         * serialize the object.
+         */
+        @Override
+        public void write(DataOutput output) throws IOException {
+            this.text.write(output);
+            this.count.write(output);
+        }
+
+        @Override
+        public String toString() {
+            return String.format("<%s, %d>", text.toString(), count.get());
+        }
+
+        @Override
+        public int hashCode() {
+            return (text.hashCode() * 7) + (count.hashCode() * 11);
+        }
+
+        @Override
+        public boolean equals(Object other) {
+            if (other != null && other instanceof CountPair) {
+                CountPair pair = (CountPair) other;
+                return text.equals(pair.text) && count.equals(pair.count);
+            } else {
+                return false;
+            }
+        }
+
+        /**
+         * Compare two writable pairs.
+         * First by string, then by count.
+         */
+        @Override
+        public int compareTo(CountPair other) {
+            if (text.compareTo(other.text) != 0) {
+                return text.compareTo(other.text); // equal
+            } else {
+                return count.compareTo(other.count);
+            }
+        }
+
+    }
 }
-
-
-
