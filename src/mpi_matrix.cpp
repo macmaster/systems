25df98368f0fab00069a63470ac01519b2b6c404
Ronny <ronnymacmaster@gmail.com>
2017-04-11 08:23:35 -0500

    clean and ready to test

--- src/mpi_matrix.cpp
+++ src/mpi_matrix.cpp
@@ -26,6 +26,7 @@ void ReadInput(vector<int> &ivector, vector< vector<int> > &imatrix);
 
 void master(int world_size);
 void slave(int rank);
+int multiply(vector<int> &v1, vector<int> &v2);
 
 int main(int argc, char **argv){
 	// initialize MPI.
@@ -127,6 +128,9 @@ void master(int procs){
 	vector< vector<int> > imatrix;
 	ReadInput(ivector, imatrix);
 	int n = imatrix.size(), m = ivector.size(); // rows, cols
+	
+	// matrix multiplication result
+	vector<int> result(n);
 
 	// // print inputs (DEBUG)
 	// printf("vector size: %d\n", ivector.size());
@@ -136,9 +140,15 @@ void master(int procs){
 	// printMatrix(imatrix);
 	// printf("\n");
 	
+	
 	if(procs - 1 == 0){
 		// solo it...
 		printf("solo it...\n");
+		// process every row
+		for(int r = 0; r < n; r++){
+			vector<int> &row = imatrix[r];
+			result[r] = multiply(row, ivector);
+		}
 	} 
 	
 	else{ // send out assignments.
@@ -156,9 +166,30 @@ void master(int procs){
 				// printf("rank 0: sending msg from %d with size %d, stride %d, extra %d\n", start + j, n, stride, extra);
 				MPI_Send(&imatrix[start + j][0], m, MPI_INT, i, j + 1, MPI_COMM_WORLD);
 			}
-
 		}
+
+		// process extra rows
+		for(int r = 0; r < extra; r++){
+			vector<int> &row = imatrix[r];
+			result[r] = multiply(row, ivector);
+		}
+
+		// collect and reduce result vector
+		for(int i = 1; i < procs; i++){
+			vector<int> values(stride);
+			MPI_Recv(&values[0], stride, MPI_INT, i, stride + 1, MPI_COMM_WORLD, MPI_STATUS_IGNORE);
+			int start = ((i - 1) * stride) + extra;
+			for(int r = 0; r < stride; r++){
+				result[start + r] = values[r];
+			}
+		}
+
 	}
+
+	// write to file
+	printf("Number of processes: %d\n", procs);
+	printf("result vector: "); printVector(result);
+	printf("\n");
 
 }
 
@@ -191,8 +222,32 @@ void slave(int rank){
 	}
 
 	// DEBUG routine:
-	printf("rank: %d. \t dim: %d x %d\n", rank, rows, cols);
+	//printf("rank: %d. \t dim: %d x %d\n", rank, rows, cols);
 	//printf("ivector:\n"); printVector(ivector);
-	printf("imatrix:\n"); printMatrix(imatrix);
+	//printf("imatrix:\n"); printMatrix(imatrix);
+	
+	// vector result of the submatrix multiplication.
+	vector<int> result(rows);
+	for(int r = 0; r < rows; r++){
+		vector<int> &row = imatrix[r];
+		result[r] = multiply(row, ivector);
+	}
 
+	// report result vector to master.
+	MPI_Send(&result[0], rows, MPI_INT, 0, rows + 1, MPI_COMM_WORLD);
+}
+
+
+/**
+ * multiplies two vectors together.
+ * the result is a scalar.
+ * returns the scalar dot product of two vectors
+ * precondition: vectors are of equal length.
+ */
+int multiply(vector<int> &v1, vector<int> &v2){
+	int result = 0;
+	for(int i = 0; i < v1.size(); i++){
+		result += v1[i] * v2[i];
+	}
+	return result;
 }
