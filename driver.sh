2280d1b8763a7d9e02edd09faf1d39de75fd4512
ronny <ronnymacmaster@gmail.com>
2017-04-23 15:20:36 -0500

    mychanges

--- driver.sh
+++ driver.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-echo "!/bin/bash" > kill.sh
+echo "#!/bin/bash" > server_kill.sh
 n=2
 for i in `seq 1 $n`; do
 	(java -cp bin/ server.Server <input/s${i}.txt >output/s${i}.txt &)
-	echo "kill $!" >> kill.sh;
+	echo "kill $!" >> server_kill.sh;
 done
 
-echo "printing kill.sh: "
-cat kill.sh
+echo "printing server_kill.sh: "
+cat server_kill.sh
 
 echo "running server driver: "
-java server.ServerDriver
+java -cp bin/ server.ServerDriver
 
-./kill.sh
-rm kill.sh
+chmod +x server_kill.sh
+./server_kill.sh
+rm server_kill.sh
